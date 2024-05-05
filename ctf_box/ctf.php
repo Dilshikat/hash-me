bbb5b97997adc6282a36bf61491ebe5c8981d226
Dishikat <dilshikatissera.uni@gmail.com>
2024-05-06 00:20:16 +0530

    Added ctf_box directory

---
+++ ctf_box/ctf.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Hash me :)</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to Hash Me!</h1>
+    There are some hashes in this web page. Can you find the hashes and guess the secret folder name?
+    <br><br>
+    
+    <button onclick="revealHash()">click me :)</button>
+     <p id="hash"></p>
+    
+</body>
+</html>
+
---
+++ ctf_box/script.js
@@ -0,0 +1,4 @@
+
+function revealHash() {
+    alert("2/3 hash value is b14a7b8059d9c055954c92674ce60032" );
+}
